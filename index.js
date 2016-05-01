bf47812549a95811e12fff57217815fe588676d7
Eric Kudler <joeento@gmail.com>
2016-05-01 14:27:41 -0400

    Now takes variable data in from args

--- index.js
+++ index.js
@@ -2,21 +2,25 @@
 
 var request = require('request');
 var cheerio = require('cheerio');
+var yargs = require('yargs');
 
-request('http://www.fandango.com/captainamerica:civilwar_185792/movietimes?location=11566&date=5%2f6%2f2016', function (error, response, html) {
+var argv = require('yargs').argv;
+console.log(argv.date);
+
+request('http://www.fandango.com/captainamerica:civilwar_185792/movietimes?location=' + argv.zip + '&date=' + argv.date, function (error, response, html) {
 	if (!error && response.statusCode === 200) {
 		var $ = cheerio.load(html);
 		$('div[itemtype="http://schema.org/MovieTheater"]').each(function(key, val) {
 			var theater = $(val);
 			var theaterName = theater.find('meta[itemprop="name"]').attr('content');
-			if (theaterName === 'AMC Levittown 10') {
+			if (theaterName === argv.theater) {
 				theater.find($('span[itemtype="http://schema.org/TheaterEvent"]')).each(function(key, val) {
 					var movie = $(val);
 					var movieName = movie.find('meta[itemprop="name"]').attr('content');
 					console.log(movieName);
 					movie.find($('meta[itemprop="startDate"]')).each(function(key, val) {
 						var showtime = $(val);
-						console.log(showtime.attr("content"));
+						console.log(showtime.attr('content'));
 					});
 				});
 				return false;
