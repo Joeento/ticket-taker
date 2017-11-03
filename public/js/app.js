d5a2f8043cc6dc828d7d00e0e392a2f289f50cb7
Eric Kudler <joeento@gmail.com>
2017-11-03 17:07:50 -0400

    Adds angular component for homepage

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+angular.module('ticketTakerApp', ['ui.router', 'ui.bootstrap']);
---
+++ public/js/home/home.component.js
@@ -0,0 +1,8 @@
+angular.
+    module('ticketTakerApp').
+    component('home', {
+        templateUrl: 'js/home/home.template.html',
+        controller: ['$state', function($state) {
+			console.log('ergreg');
+	}]
+});
