5642e1d450a85abf0792d294e60faec3ea831470
Eric Kudler <joeento@gmail.com>
2017-11-19 14:57:19 -0500

    Adds a movie chooser and a date picker for start/end range to job list form

--- public/js/job-info/job-info.component.js
+++ public/js/job-info/job-info.component.js
@@ -3,9 +3,27 @@ angular.
     component('jobInfo', {
         templateUrl: 'js/job-info/job-info.template.html',
         bindings: { jobs: '<' },
-        controller: ['$http', function($http) {
+        controller: ['$http', '$sce', function($http, $sce) {
 			var self = this;
 			console.log('yhryhj');
+
+			//autocomplete logic
+			self.movies = [];
+			self.refreshMovies = function(query) {
+				return $http.get(
+					'/api/search',
+					{
+						params: {
+							q: query
+						}
+					}
+				).then(function(response) {
+					self.movies = response.data;
+				});
+			}
+			self.trustAsHtml = function(value) {
+				return $sce.trustAsHtml(value);
+			};
 			
 	}]
 });
--- public/js/job-list/job-list.component.js
+++ public/js/job-list/job-list.component.js
@@ -5,10 +5,6 @@ angular.
         templateUrl: 'js/job-list/job-list.template.html',
         controller: ['$http', '$sce', function($http, $sce) {
 			var self = this;
-			function test() {
-				console.log(self);
-			}
-			setTimeout(test, 5000)
 			
 	}]
 });
