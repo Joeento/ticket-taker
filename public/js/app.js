3afc0f02ead93ceb3741ce21d3b9b5e9d2cb07d9
Eric Kudler <joeento@gmail.com>
2018-02-18 02:02:41 -0500

    Adds switch for toggling jobs as active/inactive in list ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('ticketTakerApp', ['ui.router', 'ui.bootstrap', 'ui.select', 'jobList', 'jobInfo', 'moment-picker']);
+angular.module('ticketTakerApp', ['ui.router', 'ui.bootstrap', 'ui.select', 'jobList', 'jobInfo', 'moment-picker', 'ui.toggle']);
--- public/js/job-list/job-list.component.js
+++ public/js/job-list/job-list.component.js
@@ -5,6 +5,19 @@ angular.
         templateUrl: 'js/job-list/job-list.template.html',
         controller: ['$http', '$sce', function($http, $sce) {
 			var self = this;
+			self.toggleJob = function(job_id) {
+				self.jobs.data.forEach(function(job) {
+					if (job._id === job_id) {
+						$http.post(
+							'/api/jobs/' + job_id + '/toggle',
+							{
+								state: job.active
+							}
+						)
+					}
+				});
+				
+			}
 			
 	}]
 });
