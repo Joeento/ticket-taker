3b6da1a8382ceef6d7499c69adde77e99f9f0b39
Eric Kudler <joeento@gmail.com>
2018-02-24 15:33:06 -0500

    Moves callback into a space where it can use the job data

--- scripts/scan.js
+++ scripts/scan.js
@@ -13,28 +13,34 @@ var Theater = require('../models/Theater');
 
 mongoose.connect(config.mongo.url);
 
-Job.find().populate('theater').exec({}, function(err, jobs) {
+Job.find().populate('movie').populate('theater').exec({}, function(err, jobs) {
     jobs.forEach(function(job) {
         var date = new Date(job.time_start);
         console.log('https://www.fandango.com/napi/theaterMovieShowtimes/' + job.theater.fandango_id + '?startDate=' + toISODate(date) + '&isdesktop=true');
         var options = {
-            url: 'https://www.fandango.com/napi/theaterMovieShowtimes/' + job.theater.fandango_id + '?startDate=' + (date) + '&isdesktop=true',
+            url: 'https://www.fandango.com/napi/theaterMovieShowtimes/' + job.theater.fandango_id + '?startDate=' + toISODate(date) + '&isdesktop=true',
             headers: {
                 'cookie': config.fandango.cookie,
                 'referer': 'https://www.fandango.com/amc-dine-in-levittown-10-AABQM/theater-page?date=2018-02-24'
             }
         };
-        request(options, callback);
+        (function(jobCopy) {
+            request(options, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var data = JSON.parse(body);
+                    data.viewModel.movies.forEach(function(movie) {
+                        
+                        if (movie.title === jobCopy.movie.name) {
+                            console.log(movie);
+                        }
+                    });
+                    
+                }
+            });
+        })(job);
     });
 });
 
-
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var data = JSON.parse(body);
-    console.log(data);
-  }
-}
 
 function toISODate(date) {
     return date.getFullYear() + '-' +
