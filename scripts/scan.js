5fc517206fbc51c9d1578004644d59477a2d3ff0
Eric Kudler <joeento@gmail.com>
2018-02-25 13:46:16 -0500

    Cycle through showtimes and compare them to Job's start and endtime

--- scripts/scan.js
+++ scripts/scan.js
@@ -27,12 +27,24 @@ Job.find().populate('movie').populate('theater').exec({}, function(err, jobs) {
                 if (!error && response.statusCode == 200) {
                     var data = JSON.parse(body);
                     data.viewModel.movies.forEach(function(movie) {
-                        
                         if (movie.title === jobCopy.movie.name) {
-                            console.log(movie);
+                            movie.variants.forEach(function(variant) {
+                                if ((!job.format && variant.format === 'Standard') || job.format === variant.format) {
+                                    variant.amenityGroups[0].showtimes.forEach(function(showtime) {
+                                        var startDate = new Date(jobCopy.time_start);
+                                        var endDate = new Date(jobCopy.time_end);
+                                        var ticketingDate = showtime.ticketingDate.replace('+', ' ');
+                                        var showtimeDate = new Date(ticketingDate);
+                                        if (startDate <= showtimeDate && showtimeDate <= endDate) {
+                                            var theater_slug = toSlug(job.theater);
+                                            var link = 'https://www.fandango.com/' + theater_slug + '/theater-page?date=' + toISODate(startDate)
+                                            console.log(link)
+                                        }
+                                    });
+                                }
+                            });
                         }
                     });
-                    
                 }
             });
         })(job);
@@ -44,4 +56,12 @@ function toISODate(date) {
     return date.getFullYear() + '-' +
         ('0'+ (date.getMonth()+1)).slice(-2) + '-' +
         ('0'+ date.getDate()).slice(-2);
+}
+
+function toSlug(result) {
+    var slug = result.name;
+    slug = slug.toLowerCase();
+    slug = slug.replace(/[\s\(\)]/g, '-');
+    slug = slug + '-' + result.fandango_id;
+    return slug;
 }
